4f260e1e36628a8c5c7e411108a98895153bce24
Troy Chryssos <github email address>
2016-08-22 13:09:12 -0400

    Wrote custom getter and setter methods to handle special form fields Wrote custon getter and setter methods that correspond to special fields in our form (namely, a genre drop down list and an artist auto-filler). These were then processed and returned as part of the data of a given song. I also modified one spec to check for the genre_name

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,53 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+    self.save
+  end
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    end
+  end
+
+  def artist_name=(name)
+    self.artist=Artist.find_or_create_by(name:name)
+    self.save
+    self.artist.save
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+  def note_contents=(contents)
+    if contents != ""
+      if contents.is_a?(String)
+        note=Note.create(content:contents)
+        self.notes<<note
+      else
+        contents.each do |content|
+          note=Note.create(content:content)
+          self.notes<<note
+        end
+      end
+    end
+  end
+
+  def note_contents
+    if self.notes
+      notes=self.notes.collect do |note|
+        unless note.content==""
+          note.content
+        end
+      end
+    end
+    notes.compact
+  end
+
+end
